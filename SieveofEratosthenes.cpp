6aadb219510294f95bc5788a9ca05e1944138c46
RRkundkar77 <r4002001005025k@gmail.com>
2020-12-14 17:08:11 +0530

    AlgoToFindPrimes

---
+++ SieveofEratosthenes.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void SieveOfEratosthenes(int n) 
+{ 
+    // Create a boolean array "prime[0..n]" and initialize 
+    // all entries it as true. A value in prime[i] will 
+    // finally be false if i is Not a prime, else true. 
+    bool primearray[n+1]; 
+    memset(primearray, true, sizeof(primearray)); 
+    
+    //Marking the multiples of Prime numbers as composites
+    for (int num=2; num*num<=n; num++) 
+    { 
+        // If prime[p] is not changed, then it is a prime 
+        if (primearray[num] == true) 
+        { 
+            // Updating all multiples of p greater than or  
+            // equal to the square of it 
+            // numbers which are multiple of p and are 
+            // less than p^2 are already been marked.  
+            for (int i=num*num; i<=n; i += num) 
+                primearray[i] = false; 
+        } 
+    } 
+  
+    // Printing all prime numbers 
+    for (int num=2; num<=n; num++) 
+       if (primearray[num]) 
+          cout << num << " "; 
+} 
