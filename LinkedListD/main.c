0d3cfe4309fa42776989e819971f637be0bc3471
RRkundkar77 <r4002001005025k@gmail.com>
2021-01-31 20:43:58 +0530

    updated limked list ADT

--- LinkedListD/main.c
+++ LinkedListD/main.c
@@ -4,11 +4,18 @@ int main(){
     dlist L1;
     init(&L1);
     append(&L1,56);
+    display(L1);
     prepend(&L1,3);
+    display(L1);
     append(&L1,2);
+    display(L1);
     append(&L1,1);
+    display(L1);
     append(&L1,0);
+    display(L1);
     delete(&L1,65);
+    delete(&L1,2);
+    display(L1);
     display(L1);
     destroy(&L1);
     display(L1);
