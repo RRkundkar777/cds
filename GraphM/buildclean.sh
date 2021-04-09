4725f7ea1c3c97e2fc32293231530db2d1c446a8
RRkundkar77 <r4002001005025k@gmail.com>
2021-04-09 22:50:30 +0530

    added prims and kruskal's MST Algo (Adjacency Matrix)

--- GraphM/buildclean.sh
+++ GraphM/buildclean.sh
@@ -1,7 +1,8 @@
 # Shell script for clean build
 gcc -c graph.c 
 gcc -c main.c 
-gcc main.o graph.o -o app
+gcc -c graphQueue.c
+gcc main.o graph.o graphQueue.o -o app
 ./app
 rm app
 rm *.o
--- GraphM/main.c
+++ GraphM/main.c
@@ -1,3 +1,5 @@
+#include<stdio.h>
+#include<limits.h>
 #include"graph.h"
 
 // Driver Code
@@ -5,22 +7,33 @@ int main(int argc, char const *argv[])
 {
     graphM g1;
     initGraph(&g1,4);
-    addEdge(&g1,0,3);
-    addEdge(&g1,1,2);
-    addEdge(&g1,2,1);
-    addEdge(&g1,3,0);
-    addEdge(&g1,3,3);
+    addScalarEdge(&g1,1,3);
+    addScalarEdge(&g1,0,1);
 
     displayGraph(g1);
-
-
 
     graphM g2;
     
     getGraphFromFile(&g2,"data.txt");
 
     displayGraph(g2);
-    
 
+    printf("BFS Traversal of Graph: ");
+    searchBFS(&g1,0);
+    printf("\n\n");
+
+    graphM g3;
+    getGraphFromFile(&g3,"primdata.txt");
+
+    primMST(&g3);
+
+    graphM g4;
+    getGraphFromFile(&g4,"kruskaldata.txt");
+
+
+    printf("\n\n");    
+    kruskalMST(&g4);
+    
+    
     return 0;
 }
