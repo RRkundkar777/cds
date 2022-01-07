7fd1dafda245d02d148516c1eb69c482cfe3b1c6
rushikesh <r4002001005025k@gmail.com>
2022-01-07 21:18:41 +0530

    completed global build script

--- gbuild.sh
+++ gbuild.sh
@@ -1,14 +1,99 @@
 # Script to build all DSA files concurrently with global_main
-
 #! /bin/bash
 
-# Make the tmp folder
-tmp()
+# Data Structure Registry
+declare -a DSA_REG
+
+# Importing useful functions
+import()
 {
-    if [ -d "./tmp" ]; 
-    then
-        :
-    else
-        mkdir tmp
-    fi
+    array_dir="./array/test.sh"
+    source $array_dir
 }
+
+# register "to be compiled" dsa folders
+register()
+{
+    DSA_REG[0]="./aa-sample-dsa"
+    DSA_REG[1]="./array"
+}
+
+# compile code files from a single dsa folder
+clangcompile()
+{
+    DSA_DIRNAME=$1
+    for file in $DSA_DIRNAME/*.cpp
+    do
+        filename=`echo "$file" | awk -F'/' '{print $3}'`
+        g++ -c $file -o ./tmp/$filename.o
+    done
+}
+
+# Compile code files from all registered dsa folders
+gnu_compile()
+{
+    for location in ${DSA_REG[@]}
+    do
+        clangcompile $location
+    done
+}
+
+# Compile global_main file
+compile_main()
+{
+    g++ -c global_main.cpp -o ./tmp/global_main.cpp.o
+}
+
+# Remove the "main.cpp.o" type files
+remove_local_main()
+{
+    rm ./tmp/main.cpp.*
+}
+
+# build the final app
+gbuild()
+{
+    g++ ./tmp/*.o -o ubd
+}
+
+# run the final app
+gnu_boot()
+{
+    ./ubd
+}
+
+# remove the .o files
+remove_dumps()
+{
+    rm -r ./tmp
+}
+
+# remove the app
+remove_app()
+{
+    rm ubd
+}
+
+# Driver Code
+main()
+{
+        if [[ "${BASH_SOURCE[0]}" == "${0}" ]]
+        then 
+            import
+            tmp
+    
+            register
+            gnu_compile
+    
+            compile_main
+            remove_local_main
+    
+            gbuild
+            gnu_boot
+
+            remove_dumps
+            remove_app
+        fi
+}
+
+main
--- global_main.cpp
+++ global_main.cpp
@@ -2,11 +2,15 @@
 
 #include<iostream>
 
+#include "aa-sample-dsa/sample.h"
 #include "array/array.h"
 
 int main(int argc, char const *argv[])
 {
     array a1(34);
     a1.mergeSort();
+
+    sample s1(56);
+    s1.mergeSort();
     return 0;
 }
