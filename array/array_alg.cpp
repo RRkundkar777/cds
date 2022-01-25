edd02b6664ac9301283b83c6caf39a9c31b6080f
rushikesh <r4002001005025k@gmail.com>
2022-01-25 09:45:42 +0530

    refactored array

--- array/array_alg.cpp
+++ array/array_alg.cpp
@@ -1,9 +1,15 @@
+/* Array algorithms */
+
 #ifndef ARRAY_ALG_H
 #define ARRAY_ALG_H
 
-#include "array.h"
+// System libraries
 #include<iostream>
 
+// Array ADT
+#include "array.h"
+
+// MergeSort algorithm
 void array::mergeSort()
 {
     std::cout << "Welcome to merge sort"<<std::endl;
--- array/array_opt.cpp
+++ array/array_opt.cpp
@@ -1,9 +1,15 @@
+/* Operations on array */
+
 #ifndef ARRAY_OPT_H
 #define ARRAY_OPT_H
 
-#include "array.h"
+// System Libraries
 #include<iostream>
 
+// Array ADT
+#include "array.h"
+
+// Constructor of array
 array::array(lint size)
 {
     this->start = new lint[size];
