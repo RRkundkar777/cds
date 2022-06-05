57f77ad8e856ff742d325680b8f8fa7e400ef514
rushikesh <r4002001005025k@gmail.com>
2022-06-05 23:58:58 +0530

    deleted constructor for array

--- array/main.cpp
+++ array/main.cpp
@@ -10,5 +10,9 @@ int main()
 {
     long long py = 4;
     array a1;
-    a1.mergeSort();
+    lint arr[] = {9,8,7,4,5,6,2,1};
+
+    // Linear search test
+    std::cout << a1.linearSearch(arr,8,899) << std::endl;
+    std::cout << a1.linearSearchImproved(arr,8,4) << std::endl;
 }
--- global_main.cpp
+++ global_main.cpp
@@ -7,15 +7,11 @@
 #include"aes-config.h"
 
 // Data Structures
-#include "aa-sample-dsa/sample.h"
 #include "array/array.h"
 
 int main(int argc, char const *argv[])
 {
-    array a1(34);
+    array a1;
     a1.mergeSort();
-
-    sample s1(56);
-    s1.mergeSort();
     return 0;
 }
