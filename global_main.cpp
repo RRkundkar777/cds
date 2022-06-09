d7c620ca10820167f0cafe7a49e2b516bb781b0a
rushikesh <r4002001005025k@gmail.com>
2022-06-09 17:31:20 +0530

    renamed gbuild and added testcases to main

--- global_main.cpp
+++ global_main.cpp
@@ -11,7 +11,10 @@
 
 int main(int argc, char const *argv[])
 {
+    vector<lint> v1{0,1,2,3,9,8,7,6};
     array a1;
-    a1.mergeSort();
+    a1.display_array(v1);
+    a1.merge_sort(v1,0,v1.size() - 1);
+    a1.display_array(v1);
     return 0;
 }
